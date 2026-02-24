7b82dcce72582249312a07806d7cb1563ef48a65
gnikesh <gnikesh03@gmail.com>
2026-02-24 10:40:16 -0600

    fix: update vite config to fix github pages generation

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   root: 'src',
-  base: '/catan-board-generator/',
+  base: '/catan-map-generator/',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
